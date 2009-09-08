2c603a7f110ccdb7b0161836f5319e56a61381f7
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2009-09-08 10:22:45 +0000

    Add geo interface to Grid.

--- pleiades.capgrids/trunk/pleiades/capgrids/__init__.py
+++ pleiades.capgrids/trunk/pleiades/capgrids/__init__.py
@@ -16,7 +16,7 @@ data = dict([(rec[0], rec[:]) for rec in list(r)[1:]])
 def box(mapid, gridsquare):
     rec = data[mapid]
     bbox = [float(rec[3]), float(rec[5]), float(rec[4]), float(rec[6])]
-    cols = [alphanums[i] for i in range(alphanums.index(rec[7].lower()), alphanums.index(rec[8].lower())+1)] 
+    cols = [alphanums[i] for i in range(alphanums.index(rec[7].lower()), alphanums.index(rec[8].lower())+1)]
     rows = [k for k in range(int(rec[9]), int(rec[10])+1)]
     m, n = len(cols), len(rows)
     dx, dy = (bbox[2] - bbox[0])/m, (bbox[3] - bbox[1])/n
@@ -27,7 +27,7 @@ def box(mapid, gridsquare):
     minx = bbox[0] + i*dx
     maxy = bbox[3] - j*dy
     return (minx, maxy-dy, minx+dx, maxy)
-    
+
 
 class Grid(object):
     
@@ -42,25 +42,30 @@ class Grid(object):
     @property
     def id(self):
         return 'http://atlantides.org/capgrids/%s/%s' % (self.mapid, self.gridsquare)
-        
+    
     @property
     def bounds(self):
         return box(self.mapid, self.gridsquare)
     
     def Title(self):
         return 'Barrington Atlas map %s, grid square %s' % (self.mapid, self.gridsquare)
-        
+    
     def Description(self):
         return 'Bounding polygon of Barrington Atlas map %s, grid square %s' % (self.mapid, self.gridsquare)
-        
+    
     def Creator(self):
         return 'Classical Atlas Project, edited by R. Talbert'
-
+    
     @property
     def type(self):
         return 'Polygon'
-        
+    
     @property
     def coordinates(self):
         l, b, r, t = self.bounds
         return (((l, b), (l, t), (r, t), (r, b), (l, b)),)
+    
+    @property
+    def __geo_interface__(self):
+        return dict(type=self.type, coordinates=self.coordinates)
+        
