5e23335ad1f620d180ebfe9c6c6136e32aed13b4
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2009-10-26 12:33:20 +0000

    Handle multi-maps, up version to 0.2.

--- pleiades/capgrids/__init__.py
+++ pleiades/capgrids/__init__.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import islice
 import os
 
 from zope.dublincore.interfaces import ICMFDublinCore
@@ -9,24 +10,38 @@ from zgeo.geographer.interfaces import IGeoreferenced
 alphanums = 'abcdefghijklmn'
 scales = {'5000': 5.0, '1000': 1.0, '500': 0.5, '150': 0.25}
 f = open(os.path.join(os.path.dirname(__file__), 'maps.csv'))
-r = csv.reader(f)
+reader = csv.reader(f)
 
-data = dict([(rec[0], rec[:]) for rec in list(r)[1:]])
+# data = dict([(rec[0], rec[:]) for rec in list(r)[1:]])
+
+data = {}
+for r in islice(reader, 1, None):
+    key = r[0]
+    value = data.get(key, [])
+    value.append(r)
+    data[key] = value
 
 def box(mapid, gridsquare):
-    rec = data[mapid]
-    bbox = [float(rec[3]), float(rec[5]), float(rec[4]), float(rec[6])]
-    cols = [alphanums[i] for i in range(alphanums.index(rec[7].lower()), alphanums.index(rec[8].lower())+1)]
-    rows = [k for k in range(int(rec[9]), int(rec[10])+1)]
-    m, n = len(cols), len(rows)
-    dx, dy = (bbox[2] - bbox[0])/m, (bbox[3] - bbox[1])/n
-    col = gridsquare[0].lower()
     row = int(gridsquare[1:])
-    i = cols.index(col)
-    j = rows.index(row)
-    minx = bbox[0] + i*dx
-    maxy = bbox[3] - j*dy
-    return (minx, maxy-dy, minx+dx, maxy)
+    col = gridsquare[0].lower()
+    for rec in data[mapid]:
+        assert rec[0] == mapid
+        try:
+            cols = [alphanums[i] for i in range(alphanums.index(rec[7].lower()), alphanums.index(rec[8].lower())+1)]
+            rows = [k for k in range(int(rec[9]), int(rec[10])+1)]
+            assert row in rows
+            assert col in cols
+            bbox = float(rec[3]), float(rec[5]), float(rec[4]), float(rec[6])
+            dx = (bbox[2] - bbox[0])/len(cols)
+            dy = (bbox[3] - bbox[1])/len(rows)
+            minx = bbox[0] + cols.index(col)*dx
+            maxy = bbox[3] - rows.index(row)*dy
+            return (minx, maxy-dy, minx+dx, maxy)
+        except AssertionError:
+            pass
+        except:
+            raise
+    raise IndexError, "No gridsquare %s in map %s" % (gridsquare, mapid)
 
 
 class Grid(object):
